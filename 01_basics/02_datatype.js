9bb6fd38009172514f1ee31142e62e4e2537f720
Samir Agrawal <samir.kr.0110@gmail.com>
2025-02-16 23:01:04 +0000

    02_datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,10 @@
+"use strict"; //to tell compiler to use newer version of java script
+
+// datatypes => string => number => boolean => underfined => null => bigint => symbol(uniqueness) =>objects
+
+console.log(typeof "samir") //string
+console.log(typeof 12) //number
+console.log(typeof false) //boolean
+console.log(typeof null) //objects
+console.log(typeof underfined) //undefined
+
