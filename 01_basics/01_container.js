427097723d4f861643a1bcbb9135e2edb2e501e1
Samir Agrawal <samir.kr.0110@gmail.com>
2025-02-16 22:32:44 +0000

    01_container

---
+++ 01_basics/01_container.js
@@ -0,0 +1,7 @@
+const Acoountid = 123098
+
+let accountEmail="samir.kr.0110@gmail.com"
+var accountPass="1234567890"
+accountCity="jaipur";
+
+console.table([accountEmail,accountPass,accountCity,Acoountid])
